466a610a3ef482dbf88233b63512643b6210c131
Cody Clawson <cclawson@student.neumont.edu>
2016-10-25 21:49:46 -0600

    fixes bullet movement

--- assets/scripts/Bullet.js
+++ assets/scripts/Bullet.js
@@ -4,8 +4,8 @@ var Bullet = function (bulletBitmap) {
     this.bulletBitmap = bulletBitmap;
     this.velocity = 0;
     this.active = true;
-    this.xVelocity = 1;
-    this.yVelocity = 1;
+    this.xVelocity = .1;
+    this.yVelocity = .1;
     this.width = 3;
     this.height = 3;
     this.color = "#000";
@@ -20,14 +20,21 @@ Bullet.prototype.draw = function () {
 };
 
 Bullet.prototype.update = function (enemy) {
-    var angle = Math.floor(toDegrees(Math.atan(enemy.y / enemy.x)));
+    var angle = Math.atan2(enemy.y - this.bulletBitmap.y, enemy.x - this.bulletBitmap.x);
+    angle = toDegrees(angle);
     if (this.active) {
-        this.bulletBitmap.x += Math.ceil(Math.cos(angle));
-        this.bulletBitmap.y += Math.ceil(Math.sin(angle));
+        var velocityX = Math.cos((angle) * Math.PI / 180) * (this.xVelocity * 1);
+        var velocityY = Math.sin((angle) * Math.PI / 180) * (this.yVelocity * 1);
+
+        this.bulletBitmap.x += velocityX;
+        this.bulletBitmap.y += velocityY;
     }
     this.active = this.active && this.inBounds();
 
-    if (!this.inBounds()) {
+    var collision = collisionMethod(this.bulletBitmap, enemy);
+
+
+    if (!this.inBounds() || collision) {
         this.bulletBitmap.x = this.startX;
         this.bulletBitmap.y = this.startY;
         this.active = true;
